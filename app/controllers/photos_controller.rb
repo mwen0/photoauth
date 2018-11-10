447c796ef6e2f78c961c21a1dba6f66a98951d7c
Raghu Betina <raghu@firstdraft.com>
2018-11-10 15:43:51 +0000

    Only photo user can modify photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,14 @@
 class PhotosController < ApplicationController
+  before_action :current_user_must_be_photo_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_photo_user
+    photo = Photo.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == photo.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @photos = Photo.all
 
